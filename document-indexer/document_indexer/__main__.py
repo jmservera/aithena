8f90da529ac96c7fa588dc712fb5e9acc419eb90
jmservera <8036360+jmservera@users.noreply.github.com>
2023-07-17 22:48:59 +0000

    linter

--- document-indexer/document_indexer/__main__.py
+++ document-indexer/document_indexer/__main__.py
@@ -10,84 +10,127 @@ from qdrant_client import models, QdrantClient
 
 from .blob_storage import BlobStorage
 
-RABBITMQ_HOST = os.environ.get('RABBITMQ_HOST', 'localhost')
-RABBITMQ_PORT = os.environ.get('RABBITMQ_PORT', 5672)
-REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
-REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
-EMBEDDINGS_HOST= os.environ.get('EMBEDDINGS_HOST', 'localhost')
-EMBEDDINGS_PORT= os.environ.get('EMBEDDINGS_PORT', 5000)
-QDRANT_HOST= os.environ.get('QDRANT_HOST', 'localhost')
-QDRANT_PORT= os.environ.get('QDRANT_PORT', 6333)
-STORAGE_ACCOUNT_NAME = os.environ.get('STORAGE_ACCOUNT_NAME')
-STORAGE_CONTAINER = os.environ.get('STORAGE_CONTAINER')
+RABBITMQ_HOST = os.environ.get("RABBITMQ_HOST", "localhost")
+RABBITMQ_PORT = os.environ.get("RABBITMQ_PORT", 5672)
+REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
+REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
+EMBEDDINGS_HOST = os.environ.get("EMBEDDINGS_HOST", "localhost")
+EMBEDDINGS_PORT = os.environ.get("EMBEDDINGS_PORT", 5000)
+QDRANT_HOST = os.environ.get("QDRANT_HOST", "localhost")
+QDRANT_PORT = os.environ.get("QDRANT_PORT", 6333)
+STORAGE_ACCOUNT_NAME = os.environ.get("STORAGE_ACCOUNT_NAME")
+STORAGE_CONTAINER = os.environ.get("STORAGE_CONTAINER")
+
 
 async def heartbeat(channel: BlockingChannel):
-    count=0
+    """Send heartbeat to RabbitMQ every 10 seconds
+    channel: BlockingChannel
+    """
+    count = 0
     while True:
-        count+=1
+        count += 1
         print(f"Waiting for embeddings {count}")
         channel.connection.process_data_events()
         await asyncio.sleep(10)
 
-async def get_embeddings_async(text,channel):
+
+async def get_embeddings_async(text, channel):
     async with aiohttp.ClientSession() as session:
-        hearbeat_task=asyncio.create_task(heartbeat(channel))
-        async with session.get(f'http://{EMBEDDINGS_HOST}:{EMBEDDINGS_PORT}/?text={text}') as resp:
+        hearbeat_task = asyncio.create_task(heartbeat(channel))
+        async with session.get(
+            f"http://{EMBEDDINGS_HOST}:{EMBEDDINGS_PORT}/?text={text}"
+        ) as resp:
             result = await resp.json()
             hearbeat_task.cancel()
-            return result['result']
-        
-def get_embeddings(text,channel):
-    return loop.run_until_complete(get_embeddings_async(text,channel))
+            return result["result"]
 
-def callback(channel: BlockingChannel, method, properties, body):
-    print(f"Received new document: {body}")
+
+def get_embeddings(text, channel):
+    return loop.run_until_complete(get_embeddings_async(text, channel))
+
+
+def callback(
+    channel: BlockingChannel,
+    method: pika.spec.Basic.Deliver,
+    properties: pika.spec.BasicProperties,
+    body: bytes,
+):
+    """Callback function for RabbitMQ consumer"""
+    msg_count=channel.get_waiting_message_count()    
+
+    count=get_queue(channel)
+    print(f"Received new document: {body}. Remaining messages: {count}")
 
     qdrant = QdrantClient(url=f"http://{QDRANT_HOST}:{QDRANT_PORT}")
     qdrant.recreate_collection(
         collection_name="documents",
         vectors_config=models.VectorParams(
-            size=4096,# todo: get from service... encoder.get_sentence_embedding_dimension(), # Vector size is defined by used model
-            distance=models.Distance.COSINE
-        )
+            size=4096,  # todo: get from service... encoder.get_sentence_embedding_dimension(), # Vector size is defined by used model
+            distance=models.Distance.COSINE,
+        ),
     )
 
-
-    filename = body.decode('utf-8')
-    if(filename.endswith('.pdf')):
+    filename = body.decode("utf-8")
+    if filename.endswith(".pdf"):
         stream = storage_client.download_blob_to_stream(STORAGE_CONTAINER, filename)
         with pdfplumber.open(stream) as pdf:
             for page in pdf.pages:
-                print(f"Processing page {page.page_number}/{len(pdf.pages)} of {filename}")          
+                print(
+                    f"Processing page {page.page_number}/{len(pdf.pages)} of {filename}"
+                )
                 text = page.extract_text()
-                text = text.split('.')
+                text = text.split(".")
                 lineNumber = 0
-                for line in text:               
+                for line in text:
                     lineNumber += 1
+                    print(f"Processing line {lineNumber}/{len(text)}")
+                    embedding = get_embeddings(line.strip() + ".", channel)
 
-                    embedding = get_embeddings(line.strip()+".",channel)
-
-                    qdrant.upsert(collection_name="documents",
-                                points=[models.PointStruct(
-                                    id=page.page_number*10000+lineNumber, #TODO: generate id
-                                    vector=embedding,
-                                    payload={"path": filename, "page": page.page_number, "line": lineNumber, "text": line}
-                                )]
+                    qdrant.upsert(
+                        collection_name="documents",
+                        points=[
+                            models.PointStruct(
+                                id=page.page_number * 10000
+                                + lineNumber,  # TODO: generate id
+                                vector=embedding,
+                                payload={
+                                    "path": filename,
+                                    "page": page.page_number,
+                                    "line": lineNumber,
+                                    "text": line,
+                                },
+                            )
+                        ],
                     )
                     channel.connection.process_data_events()
     else:
         print(f"Unsupported file type: {filename}")
-    
+
     channel.basic_ack(delivery_tag=method.delivery_tag)
+
+
+queue=None
+def get_queue(channel: BlockingChannel):
+    """Get queue from channel"""
+    global queue
+    channel.basic_qos(prefetch_count=1)
+    if queue is None:
+        queue = channel.queue_declare(queue="new_documents", durable=True, auto_delete=False)
+    else:
+        queue = channel.queue_declare(queue="new_documents", durable=True, auto_delete=False, passive=True)
+
+    return queue.method.message_count
 
 @retry(pika.exceptions.AMQPConnectionError, delay=5, jitter=(1, 3))
 def consume():
     r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
-    connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_HOST, RABBITMQ_PORT))
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(RABBITMQ_HOST, RABBITMQ_PORT)
+    )
     channel = connection.channel()
-    channel.queue_declare(queue='new_documents', durable=True, auto_delete=False)
-    
-    channel.basic_consume(queue='new_documents', on_message_callback=callback)
+    get_queue(channel)
+
+    channel.basic_consume(queue="new_documents", on_message_callback=callback)
 
     try:
         channel.start_consuming()
@@ -95,8 +138,9 @@ def consume():
     except pika.exceptions.ConnectionClosedByBroker:
         pass
 
+
 loop = asyncio.get_event_loop()
 storage_client = BlobStorage(STORAGE_ACCOUNT_NAME)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     consume()
